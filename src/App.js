a7c7b41cfc50de37fc23c89666c185828360aaae
FRAMGIA\nguyen.van.minhb <nguyen.van.minhb@sun-asterisk.com>
2019-10-07 14:46:07 +0700

    feat: login feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Example from './components/Example';
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Example />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Example />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
